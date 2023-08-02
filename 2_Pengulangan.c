c964ce508d093c90ecee98decc8f22764753e8a8
Rusdi139 <106898580+Rusdi139@users.noreply.github.com>
2023-08-02 15:02:54 +0700

    Create 2_Pengulangan.c

---
+++ 2_Pengulangan.c
@@ -0,0 +1,65 @@
+/*program pengulangan
+   Contoh program pengulangan do...while, while dan for
+   menggunakan output lampu dari pin 0 s/d 3
+*/
+void setup() {
+  // put your setup code here, to run once:
+  int a; //variabel lokal
+  //menggunakan while
+  // a = 0;
+  //  while (a <= 3)
+  //  {
+  //    pinMode(a, OUTPUT);
+  //    a = a + 1;
+  //  }
+  
+  //menggunakan do..while
+  // a = 0;
+  //  do
+  //  {
+  //    pinMode(a, OUTPUT);
+  //    a = a + 1;
+  //  } while ( a <= 3);
+  
+  //menggunakan for
+  //  for (a = 0; a <= 3; a++)
+  //  {
+  //    pinMode(a, OUTPUT);
+  //  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int a;
+  //menggunakan while
+  // a = 0;
+  //  while ( a <= 3)
+  //  {
+  //    digitalWrite(a, HIGH);
+  //    delay(500);
+  //    digitalWrite(a, LOW);
+  //    delay(500);
+  //    a = a + 1;
+  //  }
+
+  //menggunakan do..while
+  // a = 0;
+  //  do
+  //  {
+  //    digitalWrite(a, HIGH);
+  //    delay(500);
+  //    digitalWrite(a, LOW);
+  //    delay(500);
+  //    a = a + 1;
+  //  } while ( a <= 3);
+
+  //menggunakan for
+  //  for (a = 0; a <= 3; a++)
+  //  {
+  //    digitalWrite(a, HIGH);
+  //    delay(500);
+  //    digitalWrite(a, LOW);
+  //    delay(500);
+  //  }
+}
